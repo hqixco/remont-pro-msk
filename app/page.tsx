5f4cb0ddd3325818988a94a83ad4a16bfb869770
Rostislav <rostislav.krasnokutsky@gmail.com>
2026-04-21 03:40:35 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { LandingPage } from "@/components/landing-page";
 import { getSiteContent } from "@/lib/content-store";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   return <LandingPage content={await getSiteContent()} />;
 }
